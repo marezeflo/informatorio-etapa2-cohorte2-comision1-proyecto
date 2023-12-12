b92e1eb70e128d44895bef0c2b0aca576dfd29e8
marezeflo <marezeflo@gmail.com>
2023-12-12 09:52:56 -0300

    Avance views, models, forms y template

--- apps/comentarios/models.py
+++ apps/comentarios/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class Categoria(models.Model):
+
+    nombre = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.nombre
+
 class Comentario(models.Model):
 
     # usuario = models.ForeignKey(usuario,on_delete=models.CASCADE)
--- apps/publicaciones/models.py
+++ apps/publicaciones/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -13,9 +14,11 @@ class Categoria(models.Model):
 
 class Publicacion(models.Model):
 
-    # usuario = models.ForeignKey()
+    usuario = models.ForeignKey(User, on_delete = models.CASCADE)
 
     titulo = models.CharField(max_length=250)
+
+    imagen = models.ImageField(upload_to = 'publicaciones')
 
     contenido = models.TextField()
 
