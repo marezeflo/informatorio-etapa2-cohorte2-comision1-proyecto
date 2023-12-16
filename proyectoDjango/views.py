d2b6f4190746f82be75703ed821c5d8b20bb8fe6
marezeflo <marezeflo@gmail.com>
2023-12-16 16:17:58 -0300

    Linkeo de los hipervínculos y estilos correctamente, página parcialmente operativa

--- proyectoDjango/views.py
+++ proyectoDjango/views.py
@@ -6,3 +6,9 @@ def home(request):
     ultimasTresPublicaciones = Publicacion.objects.order_by('-fechaCreacion')[:3]
     contexto = {'ultimasTresPublicaciones': ultimasTresPublicaciones }
     return render(request, 'home.html', contexto)
+
+def contacto(request):
+    return render(request, 'contacto.html')
+
+def sobreNosotros(request):
+    return render(request, 'sobrenosotros.html')
